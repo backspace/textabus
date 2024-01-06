a4cc8c5363e334da5cc71bf4a70095347b553b72
Buck Doyle <b@chromatin.ca>
2024-01-06 12:49:01 -0600

    Add modified axum hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "textabus: more to come" }));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:1312").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
