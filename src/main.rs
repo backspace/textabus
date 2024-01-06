4c57f4ab44dddb87ac23578235fd66aacaa17bee
Buck Doyle <b@chromatin.ca>
2024-01-06 13:55:29 -0600

    Add Handlebars templates

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,37 @@
-use axum::{routing::get, Router};
+use axum::{extract::State, response::IntoResponse, routing::get, Router};
+use axum_template::{engine::Engine, RenderHtml};
+use handlebars::{DirectorySourceOptions, Handlebars};
+
+type AppEngine = Engine<Handlebars<'static>>;
+
+#[derive(Clone)]
+struct AppState {
+    engine: AppEngine,
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "textabus: more to come" }));
+    let mut hbs = Handlebars::new();
+    hbs.register_templates_directory(
+        "templates",
+        DirectorySourceOptions {
+            tpl_extension: ".hbs".to_string(),
+            hidden: false,
+            temporary: false,
+        },
+    )
+    .expect("Failed to register templates directory");
+
+    let app = Router::new()
+        .route("/", get(get_root))
+        .with_state(AppState {
+            engine: Engine::from(hbs),
+        });
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:1312").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn get_root(State(state): State<AppState>) -> impl IntoResponse {
+    RenderHtml("root", state.engine, ())
 }
