23d1818c5acc8e4350d18ad8cfeff20097be7e6c
Buck Doyle <b@chromatin.ca>
2024-01-14 18:57:37 -0600

    Add documentation and examples on site
    This closes #2.

--- assets/repl.js
+++ assets/repl.js
@@ -1,21 +1,52 @@
+let form;
+
 document.addEventListener("DOMContentLoaded", function () {
-  let form = document.querySelector("[data-form]");
+  activateCommands();
+
+  form = document.querySelector("[data-form]");
 
   form.addEventListener("submit", async function (e) {
-    let submitButton = document.querySelector("[data-submit]");
-    submitButton.disabled = true;
-
     e.preventDefault();
-    let formData = new FormData(form);
 
-    let result = await fetch(`/raw?body=${formData.get("body")}`, {
-      method: "GET",
-    });
-
-    let text = await result.text();
-
-    document.querySelector("textarea").value = text;
-
-    submitButton.disabled = false;
+    await submitForm();
   });
 });
+
+async function submitForm() {
+  let submitButton = document.querySelector("[data-submit]");
+  submitButton.disabled = true;
+
+  let formData = new FormData(form);
+
+  let result = await fetch(`/raw?body=${formData.get("body")}`, {
+    method: "GET",
+  });
+
+  let text = await result.text();
+
+  document.querySelector("textarea").value = text;
+
+  submitButton.disabled = false;
+}
+
+function activateCommands() {
+  document.querySelectorAll("[data-commands] li").forEach((element) => {
+    let command = element.innerText.trim();
+    let anchorElement = document.createElement("a");
+    anchorElement.href = "#";
+
+    anchorElement.addEventListener("click", executeCommand);
+    anchorElement.appendChild(element.querySelector("code"));
+
+    element.appendChild(anchorElement);
+  });
+}
+
+function executeCommand(e) {
+  e.preventDefault();
+
+  let command = e.target.innerText.trim();
+
+  document.querySelector("[data-body]").value = command;
+  submitForm();
+}
