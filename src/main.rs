25e7b8d6c8f2aa646ab964e22ada5b66b94c9507
Buck Doyle <b@chromatin.ca>
2024-01-22 22:24:03 -0600

    Change main to not be async
    This is needed for a deployment-specific thing… not ideal
    maybe but the merges are annoying.

--- src/main.rs
+++ src/main.rs
@@ -8,38 +8,45 @@ use textabus::{
     InjectableServices,
 };
 
-#[tokio::main]
-async fn main() {
-    pretty_env_logger::init();
+fn main() {
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(async {
+            pretty_env_logger::init();
 
-    let env_config_provider = EnvVarProvider::new(env::vars().collect());
-    let config = &env_config_provider.get_config();
+            let env_config_provider = EnvVarProvider::new(env::vars().collect());
+            let config = &env_config_provider.get_config();
 
-    let database_url = &config.database_url;
-    let db = PgPool::connect(database_url.as_str()).await.unwrap();
+            let database_url = &config.database_url;
+            let db = PgPool::connect(database_url.as_str()).await.unwrap();
 
-    sqlx::migrate!()
-        .run(&db)
-        .await
-        .expect("Failed to run migrations");
+            sqlx::migrate!()
+                .run(&db)
+                .await
+                .expect("Failed to run migrations");
 
-    let listener_address = "0.0.0.0:1312";
-    let listener = TcpListener::bind(listener_address)
-        .await
-        .expect("Failed to bind port 1312");
+            let listener_address = "0.0.0.0:1312";
+            let listener = TcpListener::bind(listener_address)
+                .await
+                .expect("Failed to bind port 1312");
 
-    println!("textabus listening on port 1312");
+            println!("textabus listening on port 1312");
 
-    axum::serve(
-        listener,
-        app(InjectableServices {
-            db,
-            twilio_address: Some("https://api.twilio.com".to_string()),
-            winnipeg_transit_api_address: Some("https://api.winnipegtransit.com".to_string()),
-        })
-        .await
-        .into_make_service_with_connect_info::<std::net::SocketAddr>(),
-    )
-    .await
-    .unwrap();
+            axum::serve(
+                listener,
+                app(InjectableServices {
+                    db,
+                    twilio_address: Some("https://api.twilio.com".to_string()),
+                    winnipeg_transit_api_address: Some(
+                        "https://api.winnipegtransit.com".to_string(),
+                    ),
+                })
+                .await
+                .into_make_service_with_connect_info::<std::net::SocketAddr>(),
+            )
+            .await
+            .unwrap();
+        });
 }
