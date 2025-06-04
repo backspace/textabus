493b9e83202649621e5b150795919013321596fc
Buck Doyle <b@chromatin.ca>
2025-06-03 20:37:50 -0500

    Add stops fixtures to fetch script

--- tests/fixtures/stops/fetch.sh
+++ tests/fixtures/stops/fetch.sh
@@ -17,6 +17,9 @@ fetch_and_format_api_response "locations:assiniboia%20downs.json" "locations-no-
 fetch_and_format_api_response "locations:jortleby.json" "locations-none.json"
 fetch_and_format_api_response "locations:union%20station.json" "locations.json"
 
+fetch_and_format_api_response "stops.json" "stops.json" "&x=634017&y=5527953&distance=500"
+fetch_and_format_api_response "stops.json" "stops-none.json" "&x=734017&y=5527953&distance=500"
+
 STOP_NUMBERS=(10625 10641 11052 11010 10642 10901 10902 10624 10830 10590 10907 10639 10939 10589 11051 10651 11024 10620 10675 10803 10804 10591 10158 10588 10672 10652 10157)
 
 for STOP_NUMBER in "${STOP_NUMBERS[@]}"
