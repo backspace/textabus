832137aa636f249ccb14dfde493ffe2acdb2b087
Buck Doyle <b@chromatin.ca>
2025-06-03 20:19:58 -0500

    Extract shared fetching code

--- tests/fixtures/stops/fetch.sh
+++ tests/fixtures/stops/fetch.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-curl "https://api.winnipegtransit.com/v4/locations:245%20smith.json?api-key=${API_KEY}&usage=short&effective-on=$(date +%Y-%m-%d)" | jq "." > "locations-address.json"
+BASE_URL="https://api.winnipegtransit.com/v4"
+DATE_PARAM=$(date +%Y-%m-%d)
 
-curl "https://api.winnipegtransit.com/v4/locations:portage%20%40%20main.json?api-key=${API_KEY}&usage=short&effective-on=$(date +%Y-%m-%d)" | jq "." > "locations-intersection.json"
+fetch_and_format_api_response() {
+  local endpoint=$1
+  local output_file=$2
+  local extra_query_parameters=${3:-""}
+  
+  curl "${BASE_URL}/${endpoint}?api-key=${API_KEY}&usage=short&effective-on=${DATE_PARAM}${extra_query_parameters}" | jq "." > "${output_file}"
+}
 
-curl "https://api.winnipegtransit.com/v4/locations:assiniboia%20downs.json?api-key=${API_KEY}&usage=short&effective-on=$(date +%Y-%m-%d)" | jq "." > "locations-no-stops.json"
-
-curl "https://api.winnipegtransit.com/v4/locations:jortleby.json?api-key=${API_KEY}&usage=short&effective-on=$(date +%Y-%m-%d)" | jq "." > "locations-none.json"
-
-curl "https://api.winnipegtransit.com/v4/locations:union%20station.json?api-key=${API_KEY}&usage=short&effective-on=$(date +%Y-%m-%d)" | jq "." > "locations.json"
+fetch_and_format_api_response "locations:245%20smith.json" "locations-address.json"
+fetch_and_format_api_response "locations:portage%20%40%20main.json" "locations-intersection.json"
+fetch_and_format_api_response "locations:assiniboia%20downs.json" "locations-no-stops.json"
+fetch_and_format_api_response "locations:jortleby.json" "locations-none.json"
+fetch_and_format_api_response "locations:union%20station.json" "locations.json"
 
 STOP_NUMBERS=(10625 10641 11052 11010 10642 10901 10902 10624 10830 10590 10907 10639 10939 10589 11051 10651 11024 10620 10675 10803 10804 10591 10158 10588 10672 10652 10157)
 
 for STOP_NUMBER in "${STOP_NUMBERS[@]}"
 do
-  curl "https://api.winnipegtransit.com/v4/routes.json?api-key=${API_KEY}&stop=${STOP_NUMBER}&effective-on=$(date +%Y-%m-%d)" | jq "." > "routes/stop_${STOP_NUMBER}.json"
+  fetch_and_format_api_response "routes.json" "routes/stop_${STOP_NUMBER}.json" "&stop=${STOP_NUMBER}"
 done
